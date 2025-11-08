c90ac7d165df0bf57c529c336f0f16e0f850931c
styazhkoV <alexsandr.styazhkov@gmail.com>
2025-11-08 21:34:23 +0500

    Есть одна индексная страница (index.html)  нет пока роутинга (Controllers)

---
+++ src/main/java/ru/corse/springbootstudents/SpringStudentApplications.java
@@ -0,0 +1,13 @@
+package ru.corse.springbootstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudentApplications {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudentApplications.class, args);
+    }
+
+}
---
+++ src/test/java/ru/corse/springbootstudents/SpringStudentApplicationsTests.java
@@ -0,0 +1,13 @@
+package ru.corse.springbootstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentApplicationsTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
