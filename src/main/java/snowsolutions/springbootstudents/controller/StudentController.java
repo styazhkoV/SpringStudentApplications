2be6fa2724546b0f9e1ff44794201c0935311615
styazhkoV <alexsandr.styazhkov@gmail.com>
2025-11-09 23:51:01 +0500

    pushProject

--- src/main/java/snowsolutions/springbootstudents/controller/StudentController.java
+++ src/main/java/snowsolutions/springbootstudents/controller/StudentController.java
@@ -1,17 +1,14 @@
-package ru.corse.springbootstudents.controller;
+package snowsolutions.springbootstudents.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import ru.corse.springbootstudents.model.Student;
+import java.util.List;
 
-@Controller("/")
-@RequestMapping("/api/v1/students")
+@RestController
 public class StudentController {
-    public <List> findAllStudents() {
-        return List.of(
-                Student.builder().name("Олег").email("mail@mail.com").age(34)
-        );
+    @GetMapping("/students")
+    public List<Object> findAllStudents() {
+        return List.of(new <snowsolutions.springbootstudents.controller.StudentController>) 1L, "John", "Doe", "john@example.com", 20));
     }
-
 }
--- src/main/java/snowsolutions/springbootstudents/model/Student.java
+++ src/main/java/snowsolutions/springbootstudents/model/Student.java
@@ -1,4 +1,24 @@
-package ru.corse.springbootstudents.model;
+package com.example.demo.student;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity // Для JPA, позже
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer age;
 }
